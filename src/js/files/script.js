46fed6066cd512be7105d1e489485b0c113379cf
Salosion <denis-stepurka@mail.ru>
2023-01-17 14:16:13 +0600

    added offer

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,7 +2,24 @@
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
 
-document.querySelector('.catalog-header__link').addEventListener('click', (e) => {
-  e.preventDefault();
-  document.querySelector('.catalog-header__columns').classList.toggle('active');
-});
+// Открыть каталог
+const catalogVisible = () => {
+  document.querySelector('.catalog-header__link').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('.catalog-header__columns').classList.toggle('active');
+  });
+}
+
+// Маска для телефона
+const phoneMask = () => {
+  const element = document.querySelector('.form-offer__top-phone');
+  const maskOptions = {
+    mask: '+7(000)000-00-00',
+    lazy: false,
+  }
+  const mask = new IMask(element, maskOptions);
+}
+
+phoneMask();
+catalogVisible();
+
